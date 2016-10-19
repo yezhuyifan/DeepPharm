29f5139a924cb93340d2267e601e1118a5ae9edd
yylonly <yylonly@gmail.com>
2016-10-19 23:41:17 +0800

    early stopping

--- net.mydreamy.mlpharmaceutics.oraldisintegratingtablet/src/main/java/net/mydreamy/mlpharmaceutics/oraldisintegratingtablet/TrainningDisintegrationTime.java
+++ net.mydreamy.mlpharmaceutics.oraldisintegratingtablet/src/main/java/net/mydreamy/mlpharmaceutics/oraldisintegratingtablet/TrainningDisintegrationTime.java
@@ -1,16 +1,25 @@
 package net.mydreamy.mlpharmaceutics.oraldisintegratingtablet;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
+import java.util.concurrent.TimeUnit;
 
 import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
 import org.datavec.api.split.FileSplit;
 import org.datavec.api.util.ClassPathResource;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.earlystopping.EarlyStoppingConfiguration;
+import org.deeplearning4j.earlystopping.EarlyStoppingResult;
+import org.deeplearning4j.earlystopping.saver.LocalFileModelSaver;
+import org.deeplearning4j.earlystopping.scorecalc.DataSetLossCalculator;
+import org.deeplearning4j.earlystopping.termination.MaxEpochsTerminationCondition;
+import org.deeplearning4j.earlystopping.termination.MaxTimeIterationTerminationCondition;
+import org.deeplearning4j.earlystopping.trainer.EarlyStoppingTrainer;
 import org.deeplearning4j.eval.RegressionEvaluation;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.GradientNormalization;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.Updater;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
@@ -18,6 +27,7 @@ import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.util.ModelSerializer;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
@@ -39,7 +49,7 @@ public class TrainningDisintegrationTime {
     public static final int iterations = 1;
     
     //Number of epochs (full passes of the data)
-    public static final int nEpochs = 200;
+    public static final int nEpochs = 1000;
 
     //Batch size: i.e., each epoch has nSamples/batchSize parameter updates
     public static final int batchSize = 143;
@@ -49,7 +59,7 @@ public class TrainningDisintegrationTime {
     
     public static final int numInputs = 27;
     public static final int numOutputs = 1;
-    public static final int numHiddenNodes = 50;
+    public static final int numHiddenNodes = 200;
     	
 	public static void main(String[] args) {
 		Logger log = LoggerFactory.getLogger(TrainningDisintegrationTime.class);
@@ -72,11 +82,13 @@ public class TrainningDisintegrationTime {
 		}
 
         DataSetIterator iteratortrain = new RecordReaderDataSetIterator(recordReadertrain,batchSize,27,27,true);
-        DataSet trainningData = iteratortrain.next();
+//        log.info("training set:" + trainningData.getFeatureMatrix().toString());
+//        log.info("training set:" + trainningData.getLabels().toString());
+
         
         RecordReader recordReadertest = new CSVRecordReader(numLinesToSkip,delimiter);
         try {
-        	recordReadertest.initialize(new FileSplit(new ClassPathResource("trainset.csv").getFile()));
+        	recordReadertest.initialize(new FileSplit(new ClassPathResource("testset.csv").getFile()));
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -89,13 +101,20 @@ public class TrainningDisintegrationTime {
 		}
 
         DataSetIterator iteratortest = new RecordReaderDataSetIterator(recordReadertest,20,27,27,true);
-        DataSet testData = iteratortest.next();
+
+ //       log.info("testData set:" + testData.toString());
         
-        // Normalization
-        DataNormalization normalizer = new NormalizerStandardize();
-        normalizer.fit(trainningData);           
-        normalizer.transform(trainningData); 
-        normalizer.transform(testData); 
+//        // Normalization
+//        NormalizerStandardize normalizer = new NormalizerStandardize();
+//   
+//        normalizer.fitLabel(true);
+//        normalizer.fit(trainningData); 
+//        normalizer.transform(trainningData); 
+//        log.info("training data features:\n" + trainningData.getFeatureMatrix().toString());
+//        log.info("training data label:\n" + trainningData.getLabels().toString());
+//        normalizer.transform(testData); 
+//        log.info("training data features:\n" + testData.getFeatureMatrix().toString());
+//        log.info("training data label:\n" + testData.getLabels().toString());
         
         // Network Configuration
         MultiLayerNetwork net = new MultiLayerNetwork(new NeuralNetConfiguration.Builder()
@@ -103,17 +122,29 @@ public class TrainningDisintegrationTime {
                 .iterations(iterations)
                 .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
                 .learningRate(learningRate)
-                .weightInit(WeightInit.XAVIER)
-                .regularization(true).l2(1e-4)
-                .updater(Updater.NESTEROVS).momentum(0.9)
+                .weightInit(WeightInit.RELU)
+                .regularization(true).l2(1e-3)
+               // .gradientNormalization(GradientNormalization.RenormalizeL2PerParamType)
+              //  .dropOut(0.5)
+               // .updater(Updater.NESTEROVS).momentum(0.9)
+                .updater(Updater.ADAM)
                 .list()
                 .layer(0, new DenseLayer.Builder().nIn(numInputs).nOut(numHiddenNodes)
-                        .activation("tanh")
+                        .activation("leakyrelu")
                         .build())
                 .layer(1, new DenseLayer.Builder().nIn(numHiddenNodes).nOut(numHiddenNodes)
-                        .activation("tanh")
+                        .activation("leakyrelu")
                         .build())
-                .layer(2, new OutputLayer.Builder(LossFunctions.LossFunction.MSE)
+                .layer(2, new DenseLayer.Builder().nIn(numHiddenNodes).nOut(numHiddenNodes)
+                        .activation("leakyrelu")
+                        .build())
+                .layer(3, new DenseLayer.Builder().nIn(numHiddenNodes).nOut(numHiddenNodes)
+                        .activation("leakyrelu")
+                        .build())
+                .layer(4, new DenseLayer.Builder().nIn(numHiddenNodes).nOut(numHiddenNodes)
+                        .activation("leakyrelu")
+                        .build())
+                .layer(5, new OutputLayer.Builder(LossFunctions.LossFunction.MSE)
                         .activation("identity")
                         .nIn(numHiddenNodes).nOut(numOutputs).build())
                 .pretrain(false).backprop(true).build()
@@ -121,30 +152,84 @@ public class TrainningDisintegrationTime {
         net.init();
         net.setListeners(new ScoreIterationListener(1));
         
-        //Train the network on the full data set, and evaluate in periodically
-        for( int i=0; i<nEpochs; i++ ){
-            net.fit(trainningData);
-        }
+        EarlyStoppingConfiguration esConf = new EarlyStoppingConfiguration.Builder()
+        		.epochTerminationConditions(new MaxEpochsTerminationCondition(100))
+        		//.iterationTerminationConditions(new MaxTimeIterationTerminationCondition(20, TimeUnit.MINUTES))
+        		.scoreCalculator(new DataSetLossCalculator(iteratortest, true))
+                .evaluateEveryNEpochs(1)
+        		.modelSaver(new LocalFileModelSaver("src/main/resources"))
+        		.build();
         
+        
+        
+        EarlyStoppingTrainer trainer = new EarlyStoppingTrainer(esConf, net, iteratortrain);
+
+        //Conduct early stopping training:
+        EarlyStoppingResult<MultiLayerNetwork> result = trainer.fit();
+
+       //Print out the results:
+        System.out.println("Termination reason: " + result.getTerminationReason());
+        System.out.println("Termination details: " + result.getTerminationDetails());
+        System.out.println("Total epochs: " + result.getTotalEpochs());
+        System.out.println("Best epoch number: " + result.getBestModelEpoch());
+        System.out.println("Score at best epoch: " + result.getBestModelScore());
+
+        
+        MultiLayerNetwork bestModel = null;
+        try {
+        	bestModel = ModelSerializer.restoreMultiLayerNetwork(new File("src/main/resources/bestModel.bin"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        //Get the best model:
+       //  bestModel =  result.getBestModel();
+        
+//        //Train the network on the full data set, and evaluate in periodically
+//        for( int i=0; i<nEpochs; i++ ){
+//            net.fit(iteratortrain);
+//            iteratortrain.reset();
+//        }
+        
+        iteratortrain.reset();
+        DataSet trainningData = iteratortrain.next();
+        
+        // evaluation training set
         RegressionEvaluation evalTrain = new RegressionEvaluation(1);
         
         INDArray featuresTrain = trainningData.getFeatureMatrix();
         INDArray lablesTrain = trainningData.getLabels();
         
-        INDArray PredictionTrain = net.output(featuresTrain);
+        INDArray PredictionTrain = bestModel.output(featuresTrain);
+        log.info("train label set:\n" + lablesTrain.toString());
+        log.info("train prediction set:\n" + PredictionTrain.toString());
         evalTrain.eval(lablesTrain, PredictionTrain);	  
         
-        log.info("training set R is: " + evalTrain.correlationR2(0));
+        log.info("training set R is:" + String.format("%.4f", evalTrain.correlationR2(0)));
         
+        iteratortest.reset();
+        DataSet testData = iteratortest.next();
+        
+        // evluation test set
         RegressionEvaluation evalTest = new RegressionEvaluation(1);
         
         INDArray featuresTest = testData.getFeatureMatrix();
+    //    log.info("featuresTest" + featuresTest.shapeInfoToString());
+     //   log.info("\n" + featuresTest.toString());
+
         INDArray lablesTest = testData.getLabels();
         
-        INDArray PredictionTest = net.output(featuresTest);
+        
+//        log.info(evalTest.stats());
+        INDArray PredictionTest = bestModel.output(featuresTest);
+        
+       log.info("test label value: \n" + lablesTest.toString());
+        log.info("test prediction value: \n" + PredictionTest.toString());
+
         evalTest.eval(lablesTest, PredictionTest);	  
         
-       log.info("testing set R is: " + evalTest.correlationR2(0));
+       log.info("testing set R is: " + String.format("%.4f", evalTest.correlationR2(0)));
 	}
 
 
