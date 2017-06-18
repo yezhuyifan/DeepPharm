e55104dd145c242cccdac7bc28d6961f6cda4e1d
Yilong <Yilong@10.8.212.245>
2017-06-19 04:19:54 +0800

    Transfer Learning API test

--- net.mydreamy.mlpharmaceutics.bcs/src/main/java/net/mydreamy/mlpharmaceutics/bcs/ModelTraining.java
+++ net.mydreamy.mlpharmaceutics.bcs/src/main/java/net/mydreamy/mlpharmaceutics/bcs/ModelTraining.java
@@ -25,6 +25,7 @@ import org.deeplearning4j.eval.RegressionEvaluation;
 import org.deeplearning4j.nn.api.Layer;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.ComputationGraphConfiguration;
+import org.deeplearning4j.nn.conf.GradientNormalization;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.Updater;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
@@ -37,10 +38,21 @@ import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.dataset.api.iterator.MultiDataSetIterator;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
+
+/**
+ * 
+ * @author Yilong
+ *
+ * MultTask to learning API feature extractor (without frozen layers)
+ * 
+ * Then Transfer to Each Task (frozen feature layers)
+ *
+ */
 public class ModelTraining {
 	
 	public static void main(String[] args) {
@@ -50,6 +62,7 @@ public class ModelTraining {
 		String fileDelimiter = ",";
 		int batchSize = 100;
 		
+		//caco2 reader
 		RecordReader caco2Reader = new CSVRecordReader(numLinesToSkip,fileDelimiter);
 		String caco2CsvPath = "src/main/resources/Caco2Permeability/trainingset.csv";
 		try {
@@ -61,7 +74,32 @@ public class ModelTraining {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		RecordReader caco2ValidationReader = new CSVRecordReader(numLinesToSkip,fileDelimiter);
+		String caco2ValidationCsvPath = "src/main/resources/Caco2Permeability/validationset.csv";
+		try {
+			caco2ValidationReader.initialize(new FileSplit(new File(caco2ValidationCsvPath)));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		RecordReader caco2TestingReader = new CSVRecordReader(numLinesToSkip,fileDelimiter);
+		String caco2TestingCsvPath = "src/main/resources/Caco2Permeability/testingset.csv";
+		try {
+			caco2TestingReader.initialize(new FileSplit(new File(caco2TestingCsvPath)));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
+		//logP reader
 		RecordReader logPReader = new CSVRecordReader(numLinesToSkip,fileDelimiter);
 		String logPCsvPath = "src/main/resources/logP/trainingset.csv";
 		try {
@@ -74,6 +112,31 @@ public class ModelTraining {
 			e.printStackTrace();
 		}
 
+		RecordReader logPValidationReader = new CSVRecordReader(numLinesToSkip,fileDelimiter);
+		String logPValidationCsvPath = "src/main/resources/logP/validationset.csv";
+		try {
+			logPValidationReader.initialize(new FileSplit(new File(logPValidationCsvPath)));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		RecordReader logPTestingReader = new CSVRecordReader(numLinesToSkip,fileDelimiter);
+		String logPTestingCsvPath = "src/main/resources/logP/testingset.csv";
+		try {
+			logPTestingReader.initialize(new FileSplit(new File(logPTestingCsvPath)));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//WS Reader
 		RecordReader WSReader = new CSVRecordReader(numLinesToSkip,fileDelimiter);
 		String WSPath = "src/main/resources/WaterSolubility/trainingset.csv";
 		try {
@@ -86,9 +149,41 @@ public class ModelTraining {
 			e.printStackTrace();
 		}
 		
+		RecordReader WSValidationReader = new CSVRecordReader(numLinesToSkip,fileDelimiter);
+		String WSValidationPath = "src/main/resources/WaterSolubility/validationset.csv";
+		try {
+			WSValidationReader.initialize(new FileSplit(new File(WSValidationPath)));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		RecordReader WSTestingReader = new CSVRecordReader(numLinesToSkip,fileDelimiter);
+		String WSTestingPath = "src/main/resources/WaterSolubility/testingset.csv";
+		try {
+			WSTestingReader.initialize(new FileSplit(new File(WSTestingPath)));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
         DataSetIterator logPiterator = new RecordReaderDataSetIterator(logPReader, batchSize, 9, 9, true);	
+        DataSetIterator logPValidationiterator = new RecordReaderDataSetIterator(logPValidationReader, batchSize, 9, 9, true);	
+        DataSetIterator logPTestingiterator = new RecordReaderDataSetIterator(logPTestingReader, batchSize, 9, 9, true);	
+
         DataSetIterator WSiterator = new RecordReaderDataSetIterator(WSReader, batchSize, 9, 9, true);	
+        DataSetIterator WSValidationiterator = new RecordReaderDataSetIterator(WSValidationReader, batchSize, 9, 9, true);	
+        DataSetIterator WSTestingiterator = new RecordReaderDataSetIterator(WSTestingReader, batchSize, 9, 9, true);	
+
         DataSetIterator caco2iterator = new RecordReaderDataSetIterator(caco2Reader, batchSize, 9, 9, true);	
+        DataSetIterator caco2Validationiterator = new RecordReaderDataSetIterator(caco2ValidationReader, batchSize, 9, 9, true);	
+        DataSetIterator caco2Testingiterator = new RecordReaderDataSetIterator(caco2TestingReader, batchSize, 9, 9, true);	
 
 		
 //		MultiDataSetIterator logPiterator = new RecordReaderMultiDataSetIterator.Builder(batchSize)
@@ -118,33 +213,43 @@ public class ModelTraining {
         
 		//final network
 		ComputationGraphConfiguration conf = new NeuralNetConfiguration.Builder()
-		        .learningRate(0.01)
+				.seed(123456)
 		        .graphBuilder()
 		        .addInputs("input")
-		        .addLayer("L1", new DenseLayer.Builder().nIn(9).nOut(100).build(), "input")
-		        .addLayer("L2", new DenseLayer.Builder().nIn(100).nOut(100).build(), "L1")
-		        .addLayer("L3", new DenseLayer.Builder().nIn(100).nOut(100).build(), "L2")
+		        .addLayer("L1", new DenseLayer.Builder().activation(Activation.TANH).nIn(9).nOut(100).build(), "input")
+		        .addLayer("L2", new DenseLayer.Builder().activation(Activation.TANH).nIn(100).nOut(1000).build(), "L1")
+		        .addLayer("M1", new DenseLayer.Builder().activation(Activation.TANH).nIn(1000).nOut(1000).build(), "L2")
+		        .addLayer("M2", new DenseLayer.Builder().activation(Activation.TANH).nIn(1000).nOut(1000).build(), "M1")
+		        .addLayer("M3", new DenseLayer.Builder().activation(Activation.TANH).nIn(1000).nOut(1000).build(), "M2")
+		        .addLayer("M4", new DenseLayer.Builder().activation(Activation.TANH).nIn(1000).nOut(1000).build(), "M3")	       
+		        .addLayer("L3", new DenseLayer.Builder().activation(Activation.TANH).nIn(1000).nOut(1000).build(), "M4")
 		        .addLayer("caco2", new OutputLayer.Builder().activation(Activation.SIGMOID)
-		                .lossFunction(LossFunctions.LossFunction.MSE)
-		                .nIn(100).nOut(1).build(), "L3")
+		                .lossFunction(LossFunctions.LossFunction.L2)
+		                .nIn(1000).nOut(1).build(), "L3")
 		        .addLayer("logP", new OutputLayer.Builder().activation(Activation.SIGMOID)
-		                .lossFunction(LossFunctions.LossFunction.MSE)
-		                .nIn(100).nOut(1).build(), "L3")
+		                .lossFunction(LossFunctions.LossFunction.L2)
+		                .nIn(1000).nOut(1).build(), "L3")
 		        .addLayer("WS", new OutputLayer.Builder().activation(Activation.SIGMOID)
-		                .lossFunction(LossFunctions.LossFunction.MSE)
-		                .nIn(100).nOut(1).build(), "L3")
+		                .lossFunction(LossFunctions.LossFunction.L2)
+		                .nIn(1000).nOut(1).build(), "L3")
 		        .setOutputs("caco2","logP", "WS")
+		        .backprop(true)
 		        .build();
 		
 		ComputationGraph net = new ComputationGraph(conf);
 		net.init();
-		net.setListeners(new ScoreIterationListener(1));
 		
 	    FineTuneConfiguration fineTuneConf = new FineTuneConfiguration.Builder()
 	               .learningRate(0.03)
 	               .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
 	               .updater(Updater.NESTEROVS)
+	               .momentum(0.8)
+	               .weightInit(WeightInit.XAVIER)
+//	               .updater(Updater.ADAM)
+	               .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer)
 	               .seed(123456)
+	               .regularization(true)
+	               .l2(1e-3)
 	               .build();
 		
 		//network for caco2
@@ -154,37 +259,39 @@ public class ModelTraining {
 		            .removeVertexAndConnections("logP")
 		            .build();	
 		
-		System.out.println(caco2net.summary());
+//		System.out.println(caco2net.summary());
 		caco2net.setListeners(new ScoreIterationListener(1));
 		
-			
-		for (int i = 0; i < 100; i++) {			
+		//train caco2Net	
+		for (int i = 0; i < 20; i++) {			
 			caco2net.fit(caco2iterator);    
 		}	
 		
 		//get caco2 parameters
-		Layer coco2Layer = caco2net.getLayer("caco2");
-		Map<String, INDArray> coco2 = coco2Layer.paramTable();
+		Layer caco2Layer = caco2net.getLayer("caco2");
+		Map<String, INDArray> caco2 = caco2Layer.paramTable();
 		
-		for (String s : coco2.keySet()) {
+		for (String s : caco2.keySet()) {
 			System.out.print("key: " + s);
-			System.out.println(" contents: " + coco2.get(s).toString());
+			System.out.println(" contents: " + caco2.get(s).toString());
 		}
+		
+
 		
 		//network for WS
 		ComputationGraph WSnet = new TransferLearning.GraphBuilder(caco2net)
 	            .fineTuneConfiguration(fineTuneConf)
 	            .removeVertexAndConnections("caco2")
 	            .addLayer("WS", new OutputLayer.Builder().activation(Activation.SIGMOID)
-		                .lossFunction(LossFunctions.LossFunction.MSE)
-		                .nIn(100).nOut(1).build(), "L3")
-	            .setOutputs("WS")
+		                .lossFunction(LossFunctions.LossFunction.L2)
+		                .nIn(1000).nOut(1).build(), "L3")
+	            .setOutputs("WS") 
 	            .build();	
 		
-		System.out.println(WSnet.summary());
-		WSnet.setListeners(new ScoreIterationListener(1));
+//		System.out.println(WSnet.summary());
+		WSnet.setListeners(new ScoreIterationListener(4));
 				
-		for (int i = 0; i < 100; i++) {			
+		for (int i = 0; i < 20; i++) {			
 			WSnet.fit(WSiterator);    
 		}
 		
@@ -197,20 +304,23 @@ public class ModelTraining {
 			System.out.println(" contents: " + WSpara.get(s).toString());
 		}
 		
+
+		
 		//network for logP
 		ComputationGraph logPnet = new TransferLearning.GraphBuilder(WSnet)
 	            .fineTuneConfiguration(fineTuneConf)
 	            .removeVertexAndConnections("WS")
 	            .addLayer("logP", new OutputLayer.Builder().activation(Activation.SIGMOID)
-		                .lossFunction(LossFunctions.LossFunction.MSE)
-		                .nIn(100).nOut(1).build(), "L3")
+		                .lossFunction(LossFunctions.LossFunction.L2)
+		                .nIn(1000).nOut(1).build(), "L3")
 	            .setOutputs("logP")
 	            .build();	
 		
-		System.out.println(logPnet.summary());
-		logPnet.setListeners(new ScoreIterationListener(1));
-				
-		for (int i = 0; i < 100; i++) {			
+//		System.out.println(logPnet.summary());
+		logPnet.setListeners(new ScoreIterationListener(5));
+		
+		//train logPnet
+		for (int i = 0; i < 20; i++) {			
 			logPnet.fit(logPiterator);    
 		}
 		
@@ -222,27 +332,77 @@ public class ModelTraining {
 			System.out.print("key: " + s);
 			System.out.println(" contents: " + logPpara.get(s).toString());
 		}
-		
+			
+		//evaluate Caco2Net
+		System.out.println("Caco2Net Training set accurary is :");
+		evalR(caco2iterator, caco2net);
+		System.out.println("Caco2Net validation set accurary is :");
+		evalR(caco2Validationiterator, caco2net);
+		System.out.println("");
+
+		//evaluate WSnet
+		System.out.println("WSnet Training set accurary is :");
+		evalR(WSiterator, WSnet);
+		System.out.println("WSnet validation set accurary is :");
+		evalR(WSValidationiterator, WSnet);
+		System.out.println("");
+
+		//evaluate logPnet
+		System.out.println("logPnet Training set accurary is :");
+		evalR(logPiterator, logPnet);
+		System.out.println("logPnet validation set accurary is :");
+		evalR(logPValidationiterator, logPnet);
+		System.out.println("");
+
 //		//final model
 		ComputationGraph finalnet = new TransferLearning.GraphBuilder(logPnet)
 	            .fineTuneConfiguration(fineTuneConf)
+//	            .setFeatureExtractor("L3")
 	            .removeVertexAndConnections("logP")
 	            .addLayer("WS", new OutputLayer.Builder().activation(Activation.SIGMOID)
-		                .lossFunction(LossFunctions.LossFunction.MSE)
-		                .nIn(100).nOut(1).build(), "L3")
-	            .setOutputs("logP")
+		                .lossFunction(LossFunctions.LossFunction.L2)
+		                .nIn(1000).nOut(1).build(), "L3")
+	            .setOutputs("WS")
 	            .build();	
 		
 		finalnet.getLayer("WS").setParamTable(WSpara);
 		
+		//train caco2Net	
+		for (int i = 0; i < 20; i++) {			
+			finalnet.fit(WSiterator);    
+		}
 		
+		//evaluate WSnet
+		System.out.println("fINAL WSnet Training set accurary is :");
+		evalR(WSiterator, finalnet);
+		System.out.println("FINAL WSnet validation set accurary is :");
+		evalR(WSValidationiterator, finalnet);
+		System.out.println("");
+	}
+	
+	
+	//Evaluation Function
+	public static void evalR(DataSetIterator iter, ComputationGraph net) {
 		
-//		
-//		System.out.println(logPnet.summary());
-//		logPnet.setListeners(new ScoreIterationListener(1));
-//				
-//		for (int i = 0; i < 100; i++) {			
-//			logPnet.fit(logPiterator);    
-//		}
+		 RegressionEvaluation e = new RegressionEvaluation(1);
+		
+		 iter.reset();
+		 
+		 while(iter.hasNext()) {
+			 DataSet data = iter.next();
+			 INDArray input = data.getFeatureMatrix();
+//			 System.out.println("Input 1:" + data.getFeatureMatrix().getRow(0));
+//			 System.out.println("Input 2:" + data.getFeatureMatrix().getRow(1));
+	//		 System.out.println("Input Shapre" + input.shapeInfoToString());
+			 INDArray[] output = net.output(input);
+	//		 System.out.println("Label: " + data.getLabels());
+	//		 System.out.println("Prediction: " + output[0]);
+			 e.eval(data.getLabels(), output[0]);
+		 }
+		 
+		 iter.reset();
+		 
+		 System.out.println("testing set R is: " + String.format("%.4f", e.correlationR2(0))); 
+		 
 	}
 }
