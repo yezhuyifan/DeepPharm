e11cf43d0ed4b31447ff797e2a724045af5a8676
yylonly <yylonly@gmail.com>
2017-09-01 16:08:06 +0800

    init multitask

---
+++ net.mydreamy.mlpharmaceutics.multitask/src/main/java/net/mydreamy/mlpharmaceutics/bcs/testNan.java
@@ -0,0 +1,13 @@
+package net.mydreamy.mlpharmaceutics.bcs;
+
+public class testNan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a = Double.NaN;
+		double b = 0;
+		
+		System.out.print(String.valueOf(a-b));
+	}
+
+}
